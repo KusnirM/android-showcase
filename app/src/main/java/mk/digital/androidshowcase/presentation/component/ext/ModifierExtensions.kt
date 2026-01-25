8078801e33af8fe96f7910250670a4120ce2e673
Miroslav Kusnir <mir.kusnir@gmail.com>
2026-01-25 10:07:25 +0100

    screen-previews-and-shot-tests-

--- app/src/main/java/mk/digital/androidshowcase/presentation/component/ext/ModifierExtensions.kt
+++ app/src/main/java/mk/digital/androidshowcase/presentation/component/ext/ModifierExtensions.kt
@@ -1,11 +1,24 @@
 package mk.digital.androidshowcase.presentation.component.ext
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+
+/**
+ *  Checks if the composable is being rendered from a test or not
+ */
+val LocalTestMode = staticCompositionLocalOf { false }
+
+@Composable
+fun Modifier.verticalSafeScroll(state: ScrollState = rememberScrollState()) =
+    then(if (!LocalTestMode.current) Modifier.verticalScroll(state) else Modifier)
 
 @Composable
 fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
